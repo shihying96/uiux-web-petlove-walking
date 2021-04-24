e43148a9ef69699df5ad4538639f7bf66cc7355e
shihying96 <80135963+shihying96@users.noreply.github.com>
2021-04-25 01:19:16 +0800

    0425

--- js/walkingservice/mobile/custom.js
+++ js/walkingservice/mobile/custom.js
@@ -1,10 +1,3 @@
-$(document).ready(function () {
-    $('.n_walkingReWrite').on('click', function () {
-        $('.n_walkingReWrite').toggleClass('.n_walkingChangeBox');
-    });
-});
-
-
 $(".n_slider").owlCarousel({
     // loop: true,
     // autoplay: true,
